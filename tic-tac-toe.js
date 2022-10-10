1ebda986047ffdcd22128ca5d501243db60302f2
Aybarineecm <aybarineecampbellmendez@gmail.com>
2022-10-10 00:26:28 -0500

    added excercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,33 @@
+/* Exercise 1 */
+
+window.onload= function () 
+{
+
+//let board= document.getElementById("board"); //
+let squares= document.querySelectorAll("#board div"); 
+/*the document.write() function. This is what we need to use to write text into our HTML document */
+for (let i in squares)
+{
+squares[i].classList.add("square"); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
